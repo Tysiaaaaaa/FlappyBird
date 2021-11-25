208dbd85b990db5aced7fa49ee6ed83e098471b4
STUDZPSB\mflorczak250 <d>
2021-11-25 16:56:41 +0100

    generatorPrzeszkód

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    public GameObject przeszkoda;
+    void Start()
+    {
+        
+    }
+
+    void StworzPrzeszkode()
+    {
+        
+    }
+}
